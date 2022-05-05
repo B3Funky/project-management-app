e87dd556d06c85836e9785522fcd82a3e5b7e40e
B3Funky <tyutin.s@list.ru>
2022-05-06 01:26:26 +0300

    feat: add Board page

---
+++ src/pages/Board/Board.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Header } from '../../components/Header';
+
+import './board.css';
+
+export function Board() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Board Page</h1>
+      </main>
+    </>
+  );
+}
