c1447d5d8fc556cc0ff8db40931f389416834fe6
B3Funky <tyutin.s@list.ru>
2022-05-08 01:52:39 +0300

    feat: add icons-material, add about and show-team on Welcome page

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Button } from '@mui/material';
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 
 import { Background } from '../../components/Background';
 import { paths } from '../../routes/paths';
 
 import './welcome.css';
 
+const text = {
+  buttons: {
+    login: 'Login',
+    signUp: 'Sign up',
+  },
+  h1: 'Project Management App',
+  h2: 'by Team 50',
+  about:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  showTeam: 'Show our team:',
+};
+
 export function Welcome() {
   return (
     <>
       <header className={'welcome-header'}>
         <Button component={NavLink} to={paths.login} variant="contained" size={'large'}>
-          Login
+          {text.buttons.login}
         </Button>
         <Button component={NavLink} to={paths.login} variant="contained" size={'large'}>
-          Sign up
+          {text.buttons.signUp}
         </Button>
       </header>
       <main>
-        <h1 className={'welcome-h1'}>Welcome Page</h1>
+        <h1 className={'welcome-h1'}>{text.h1}</h1>
+        <h2>{text.h2}</h2>
+        <div className={'about'}>
+          <p>{text.about}</p>
+        </div>
+        <a className={'show-team'} href={'#'}>
+          <p>{text.showTeam}</p>
+          <DoubleArrowIcon className={'show-team-arrow'} fontSize={'large'}></DoubleArrowIcon>
+        </a>
         <Background></Background>
       </main>
     </>
