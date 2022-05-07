1dc7cf34a21fb81ca099de32e213e54797ed632a
B3Funky <tyutin.s@list.ru>
2022-05-07 22:49:08 +0300

    feat: update Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,5 +3,26 @@ import React from 'react';
 import './footer.css';
 
 export function Footer() {
-  return <footer></footer>;
+  function githubAccount(name: string, link: string) {
+    return (
+      <a className={'link-to-github'} href={link} target={'blank'}>
+        <div className={'git-icon'}></div>
+        <p>{name}</p>
+      </a>
+    );
+  }
+
+  return (
+    <footer>
+      <a className={'rss-link'} href={'https://rs.school/react/'} target={'blank'} />
+      <div className={'developers'}>
+        <p>Devs:</p>
+        <div className={'github-accounts'}>
+          {githubAccount('B3Funky', 'https://github.com/B3Funky')}
+          {githubAccount('Anasstassia', 'https://github.com/Anasstassia')}
+        </div>
+      </div>
+      <p className={'year'}>©2022</p>
+    </footer>
+  );
 }
