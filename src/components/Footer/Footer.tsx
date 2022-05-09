c1481936f2de74b8e94c68b7e4c74d7d33ea81a2
B3Funky <tyutin.s@list.ru>
2022-05-09 19:09:01 +0300

    refactor: rename css classes in Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,24 +5,24 @@ import './footer.css';
 export function Footer() {
   function githubAccount(name: string, link: string) {
     return (
-      <a className={'link-to-github'} href={link} target={'blank'}>
-        <div className={'git-icon'}></div>
+      <a className={'footer-link-to-github'} href={link} target={'blank'}>
+        <div className={'footer-link-to-github-icon'}></div>
         <p>{name}</p>
       </a>
     );
   }
 
   return (
-    <footer>
-      <a className={'rss-link'} href={'https://rs.school/react/'} target={'blank'} />
-      <div className={'developers'}>
+    <footer className={'footer'}>
+      <a className={'footer-rss-link'} href={'https://rs.school/react/'} target={'blank'} />
+      <div className={'footer-developers'}>
         <p>Devs:</p>
-        <div className={'github-accounts'}>
+        <div className={'footer-github-accounts'}>
           {githubAccount('B3Funky', 'https://github.com/B3Funky')}
           {githubAccount('Anasstassia', 'https://github.com/Anasstassia')}
         </div>
       </div>
-      <p className={'year'}>©2022</p>
+      <p className={'footer-year'}>©2022</p>
     </footer>
   );
 }
