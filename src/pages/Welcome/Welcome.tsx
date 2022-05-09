f45b45e832b201989b4e7aa80a6770808f537e50
B3Funky <tyutin.s@list.ru>
2022-05-09 17:51:51 +0300

    feat: move teamMemberCard to a separate component, move text from Welcome page to a separate file

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,86 +1,18 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
-import { Button, Paper } from '@mui/material';
+import { Button } from '@mui/material';
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 
 import { Background } from '../../components/Background';
+import { TeamMemberCard } from '../../components/TeamMemberCard';
 import { paths } from '../../routes/paths';
+import text from './text.json';
 
 import './welcome.css';
 
-interface ITeamMember {
-  name: string;
-  role: string;
-  githubName: string;
-  githubUrl: string;
-  githubAvatar: string;
-}
-
-const text = {
-  buttons: {
-    login: 'Login',
-    signUp: 'Sign up',
-  },
-  h1: 'Project Management App',
-  h2: 'by Team 50',
-  about:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  showTeam: 'Show our team:',
-  showAbout: 'About this project',
-  team: [
-    {
-      name: 'Sergei Tiutin',
-      role: 'TeamLead, Coding',
-      githubName: 'B3Funky',
-      githubUrl: 'https://github.com/B3Funky',
-      githubAvatar: 'https://avatars.githubusercontent.com/u/50629262?v=4',
-    },
-    {
-      name: 'Anastasia Gubina',
-      role: 'Coding',
-      githubName: 'Anasstassia',
-      githubUrl: 'https://github.com/Anasstassia',
-      githubAvatar: 'https://avatars.githubusercontent.com/u/74318575?v=4',
-    },
-    {
-      name: 'Name Surname',
-      role: '???',
-      githubName: 'GitHub Name',
-      githubUrl: 'https://github.com/',
-      githubAvatar: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-    },
-  ],
-  mentor: {
-    name: 'Mikhail Chernyshenko',
-    role: 'Mentor',
-    githubName: 'chermic',
-    githubUrl: 'https://github.com/chermic',
-    githubAvatar: 'https://avatars.githubusercontent.com/u/29860818?v=4',
-  },
-};
-
 export function Welcome() {
   const [showTeam, setShowTeam] = useState(false);
-
-  function teamMemberCard(member: ITeamMember, key: number) {
-    return (
-      <a key={key} className={'team-member-link-wrapper'} href={member.githubUrl} target="blank">
-        <Paper className={'team-member'} elevation={3}>
-          <img
-            className={'git-avatar'}
-            src={member.githubAvatar}
-            alt={`${member.githubName} GitHub avatar image`}
-          ></img>
-          <div className={'team-member-info'}>
-            <div>{member.name}</div>
-            <div>{member.githubName}</div>
-            <div>{member.role}</div>
-          </div>
-        </Paper>
-      </a>
-    );
-  }
 
   return (
     <>
@@ -123,11 +55,11 @@ export function Welcome() {
             <p>Dev.team</p>
             <div className={'welcome-dev-team'}>
               {text.team.map((member, i) => {
-                return teamMemberCard(member, i);
+                return <TeamMemberCard key={i} {...member}></TeamMemberCard>;
               })}
             </div>
             <p>Mentor</p>
-            {teamMemberCard(text.mentor, 3)}
+            <TeamMemberCard {...text.mentor}></TeamMemberCard>
           </div>
         )}
 
