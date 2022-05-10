f69f7a5f1db0b87842305e4e1d70512f89b26273
Anasstassia <gubinaa75@gmail.com>
2022-05-10 17:12:23 +0400

    feat: add material-ui and axios requests

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -6,15 +6,23 @@ import { paths } from '../../routes/paths';
 import { Header } from '../../components/Header';
 
 import './main.css';
+import { Grid } from '@mui/material';
 
 export function Main() {
   return (
     <>
       <Header />
-      <main>
+      <Grid
+        height="100%"
+        container
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
         <h1>Main Page</h1>
         <NavLink to={paths.board}>to Board page</NavLink>
-      </main>
+      </Grid>
     </>
   );
 }
--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 import { paths } from '../../routes/paths';
 
 import './welcome.css';
