717e603949ded847cda39fcf7ece6eedd88eab50
bazla <dobr1nya@mail.ru>
2022-05-20 11:49:26 +0300

    refactor: change ColumnTitle component

--- src/components/TextAreaComponent/TextAreaComponent.tsx
+++ src/components/TextAreaComponent/TextAreaComponent.tsx
@@ -1,15 +1,23 @@
 import React, { ChangeEvent } from 'react';
-import './taskColumnTitle.css';
+import './text-area-component.css';
 
-interface ITaskColumnTitle {
-  title: string;
+interface ITextAreaComponent {
+  title?: string;
   value?: string;
+  className?: string;
   onFocus?: () => void;
   onBlur?: () => void;
   onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export const TaskColumnTitle = ({ title, value, onFocus, onBlur, onChange }: ITaskColumnTitle) => {
+export const TextAreaComponent = ({
+  title,
+  value,
+  className,
+  onFocus,
+  onBlur,
+  onChange,
+}: ITextAreaComponent) => {
   const textAreaAdjust = (el: HTMLTextAreaElement) => {
     el.style.height = '1px';
     el.style.height = el.scrollHeight + 'px';
@@ -18,7 +26,7 @@ export const TaskColumnTitle = ({ title, value, onFocus, onBlur, onChange }: ITa
   return (
     <textarea
       defaultValue={title}
-      className="column__title"
+      className={className}
       onChange={(e) => {
         textAreaAdjust(e.target);
       }}
--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -6,6 +6,7 @@ import { paths } from '../../routes/paths';
 import { Header } from '../../components/Header';
 
 import './board.css';
+import { TasksColumn } from '../../components/TasksColumn';
 
 export function Board() {
   return (
@@ -14,6 +15,7 @@ export function Board() {
       <main className={'board-main'}>
         <h1>Board Page</h1>
         <NavLink to={paths.main}>back to Main page</NavLink>
+        <TasksColumn title="Title 1" />
       </main>
     </>
   );
